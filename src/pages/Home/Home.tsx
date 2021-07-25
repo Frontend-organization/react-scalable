e3b625a98eb060984994b5c13c42c4519f4c83da
Manuel Garcia <manueldev250799@gmail.com>
2021-07-24 23:20:13 -0500

    chore: add eslint and prettier

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,13 @@
-import React from "react"
+//TODO: add react 17 jsx transform config
+import React from 'react'
 
-import Taza from "../../assets/avatar.jpg"
-
-import styles from "./Home.module.css"
+import Taza from '../../assets/avatar.jpg'
+import styles from './Home.module.css'
 
 const Home = () => {
   return (
     <section className={styles.container}>
       <h2>Hello from Home.tsx</h2>
-      <img src={Taza} alt="taza" />
     </section>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
+//FIXME: const is reserved keyword
 const { merge } = require('webpack-merge')
-
 const defaultConfig = require('./config/webpack.default.config')
 const devConfig = require('./config/webpack.dev.config')
 
 /** @type {import('webpack').Configuration} */
 module.exports = (env = 'production') => {
   const isProd = env === 'production'
-  if (isProd) return merge(defaultConfig, { mode: env })
-  return merge(defaultConfig, devConfig, { mode: env })
+  if (isProd) return merge(defaultConfig, { mode: 'production' })
+  return merge(defaultConfig, devConfig, { mode: 'development' })
 }
