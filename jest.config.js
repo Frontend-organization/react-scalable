abc80e7c63ed4ffed1776125d36905ee92ba2610
Manuel Garcia <manueldev250799@gmail.com>
2021-07-31 21:59:09 -0500

    fix: mock to svgfiles

--- jest.config.js
+++ jest.config.js
@@ -73,9 +73,10 @@ module.exports = {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less)$': 'identity-obj-proxy',
+    '\\.svg$': '<rootDir>/__mocks__/svgrMock.js',
     '^@services(.*)$': '<rootDir>/src/services$1',
     '^@components(.*)$': '<rootDir>/src/components$1',
     '^@context(.*)$': '<rootDir>/src/context$1',
